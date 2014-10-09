d2a1a8ccc8d4e39701e4b568b4b1bec857427b82
Ye Lin Aung <me@yelinaung.com>
2014-10-09 14:42:03 +0630

    let's set the pg username to admin

--- lib/devcon/repo.ex
+++ lib/devcon/repo.ex
@@ -2,7 +2,7 @@ defmodule Repo do
   use Ecto.Repo , adapter: Ecto.Adapters.Postgres
 
   def conf do
-    parse_url "ecto://tal:postgres@localhost/devcon"
+    parse_url "ecto://admin:postgres@localhost/devcon"
   end
 
   def priv do
